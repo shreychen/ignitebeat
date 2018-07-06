2af041cd355d2b461189765969c8f14c8a6571b9
sychen <sychen@aerohive.com>
2018-07-06 15:01:48 +0800

    add sql query settings

--- config/config.go
+++ config/config.go
@@ -5,6 +5,12 @@ package config
 
 import "time"
 
+type Query struct {
+	CacheName string   `yaml:"cache_name"`
+	PageSize  int      `yaml:"page_size"`
+	Sqls      []string `yaml:"sqls"`
+}
+
 type Config struct {
 	Period      time.Duration `config:"period"`
 	Server      string        `config:"server"`
@@ -12,12 +18,15 @@ type Config struct {
 	CacheMetric bool          `config:"cache_metric"`
 	AllCache    bool          `config:"all_cache"`
 	CacheList   []string      `config:"cache_list"`
+	SQL         bool          `config:"sql"`
+	Queries     []Query       `config:"queries"`
 }
 
 var DefaultConfig = Config{
 	Period:      10 * time.Second,
-	Server:      "http://localhost:8080",
+	Server:      "http://127.0.0.1:8080",
 	NodeMetric:  true,
-	CacheMetric: true,
+	CacheMetric: false,
 	AllCache:    true,
+	SQL:         false,
 }
